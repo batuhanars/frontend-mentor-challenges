4a43fa8a36a679430fad22abb91c8ba686056349
batuhanarslan <batuhan.ars@yahoo.com>
2025-10-20 13:10:27 +0300

    Revize isActive değişikeni kaldırıldı gereksiz kod temizlendi.

--- faq-accordion-main/index.js
+++ faq-accordion-main/index.js
@@ -1,5 +1,4 @@
 const accordionItems = document.querySelectorAll(".accordion-item");
-let isActive = 0;
 
 accordionItems.forEach((item) => {
   const actionLink = item.querySelector(".action-link");
@@ -8,22 +7,19 @@ accordionItems.forEach((item) => {
     accordionItems.forEach((i) => {
       if (i !== item) {
         i.querySelector(".accordion-item-body").classList.remove("active");
-        isActive = 0;
-        if (isActive == 0) {
-          i.querySelector(".icon-minus").classList.add("passive");
-          i.querySelector(".icon-minus").classList.remove("active");
-          i.querySelector(".icon-plus").classList.add("active");
-          i.querySelector(".icon-plus").classList.remove("passive");
-        }
+
+        i.querySelector(".icon-minus").classList.add("passive");
+        i.querySelector(".icon-minus").classList.remove("active");
+        i.querySelector(".icon-plus").classList.add("active");
+        i.querySelector(".icon-plus").classList.remove("passive");
       }
     });
-    isActive = 1;
-    if (isActive === 1) {
-      item.querySelector(".icon-minus").classList.remove("passive");
-      item.querySelector(".icon-minus").classList.add("active");
-      item.querySelector(".icon-plus").classList.remove("active");
-      item.querySelector(".icon-plus").classList.add("passive");
-    }
+
+    item.querySelector(".icon-minus").classList.remove("passive");
+    item.querySelector(".icon-minus").classList.add("active");
+    item.querySelector(".icon-plus").classList.remove("active");
+    item.querySelector(".icon-plus").classList.add("passive");
+
     if (item.children[1].className == "accordion-item-body active") {
       actionLink.parentElement.parentElement.childNodes[3].childNodes[1].classList.remove(
         "passive"
